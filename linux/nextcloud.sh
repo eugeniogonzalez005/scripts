eb05b2e874e2680cb1b2841d2a8ff5058bc2d4fc
yo1 <eugeniogonzalez005@gmail.com>
2021-06-20 12:55:28 +0200

    update 20-06-21

--- linux/nextcloud.sh
+++ linux/nextcloud.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # work in progress
-# Eugenio Gonzalez
-:'
+# Author: Eugenio Gonzalez
+: '
     ______                       _          ______                        __
    / ____/_  ______ ____  ____  (_)___     / ____/___  ____  ____  ____ _/ /__  ____
   / __/ / / / / __ `/ _ \/ __ \/ / __ \   / / __/ __ \/ __ \/_  / / __ `/ / _ \/_  /
@@ -10,24 +10,132 @@
             /____/
 
 '
+# ==== COLORS ====
+RED="\e[31m"
+GREEN="\e[32m"
+NOCOLOR="\e[0m"
+
+# ==== MAIN FUNCTIONS ====
+
+prerequisites () {
+    local php apache mariadb
+    php=(php php-curl php-xml php-gd php-mbstring php-zip php-mysql php-bz2 php-intl php-imap php-memcached php-imagick php-bcmath php-gmp)
+    apache=(apache2 libapache2-mod-php)
+    mariadb=(mariadb-server)
+    apt update
+    apt install --assume-yes php "${php[@]}" "${apache[@]}" "${mariadb[@]}"
+    mysql_secure_installation
+    systemctl restart mariadb && systemctl status mariadb || printf 'error: cant reload mariadb'
+    systemctl restart apache2 && systemctl status apache2 || printf 'error: cant reload apache2'
+    return
+}
+
+site_configuration () {
+    a2dissite 000-default.conf
+    mv '/etc/apache2/sites-available/000-default.conf' "/etc/apache2/sites-available/${SITE}.conf" # debian version
+    a2ensite "${SITE}.conf"
+    systemctl restart apache2
+    a2enmod rewrite
+    a2enmod headers
+    a2enmod env
+    a2enmod dir
+    a2enmod mime
+    return
+}
+
+nextcloud_install () {
+    cd "${NEXTCLOUD_DIR}" && wget https://download.nextcloud.com/server/installer/setup-nextcloud.php
+    return
+}
+
+usage () {
+    local SCRIPTNAME
+    SCRIPTNAME="$(basename "$0")"
+    printf "%s- usage: %s [-s site_name | -n nextcloud_directory | -i]\nexample: %s -s nextcloud.test -n nextcloud" "$SCRIPTNAME" "$SCRIPTNAME" "$SCRIPTNAME"
+    return
+}
+
+checker () {
+    return
+}
+
+interactive () {
+    printf 'Site name > '
+    read -re SITE
+    printf "Nextcloud directory > "
+    read -re NEXTCLOUD_DIR
+    return
+}
+
+# ==== OPTIONS ====
+
 SITE=''
-apt update &&\
-apt install --assume-yes php \
-   php-curl php-xml php-gd php-mbstring php-zip php-mysql php-bz2 php-intl php-imap php-memcached php-imagick php-bcmath php-gmp \
-   apache2 libapache2-mod-php \
-   mariadb-server && \
-mysql_secure_installation && \
-systemctl restart mariadb &&  systemctl status mariadb && \
-systemctl restart apache2 &&  systemctl status apache2 && \
-a2dissite 000-default.conf  && \
-mkdir -p '/var/www/html/nextcloud' && \
-mv '/etc/apache2/sites-available/000-default.conf' '/etc/apache2/sites-available/${SITE}.conf' && \
-a2ensite "${SITE}.conf" && \
-systemctl restart apache2 && \
-a2enmod rewrite && \
-a2enmod headers && \
-a2enmod env && \
-a2enmod dir && \
-a2enmod mime && \
-chown -R www-data:www-data '/var/www/html/nextcloud' &&  chmod -R 770 '/var/www/html/nextcloud' && \
-cd /var/www/html/nextcloud/ && wget https://download.nextcloud.com/server/installer/setup-nextcloud.php && \
+NEXTCLOUD_DIR=''
+if [[ -n "$1" ]]; then
+    while [[ -n "$1" ]]; do
+        case "$1" in
+            -s | --site)
+                shift
+                SITE="$1"
+                ;;
+            -n | --nextclou_dir)
+                shift
+                NEXTCLOUD_DIR="$1"
+                ;;
+            -i | --interactive)
+                interactive
+                ;;
+            -h | --help)
+                usage
+                ;;
+            *)
+                usage >&2
+                exit 1
+                ;;
+        esac
+        shift
+    done
+else
+    interactive
+fi
+
+if [[ -n "$SITE" && -n "$NEXTCLOUD_DIR" ]]; then
+    while true; do
+        if [[ "$NEXTCLOUD_DIR" =~ ^[-[:alnum:]_]+$ ]] && [[ "$SITE" =~ ^[-\.[:alnum:]_]+$ ]]; then
+                clear
+                printf "=> Site: %s\n=> Nextcloud directory: %s\n" "$SITE" "$NEXTCLOUD_DIR"
+                printf 'Proceed? [Y/n/q] > '
+                read -r
+                case $REPLY in
+                    Y|y)
+                        break
+                        ;;
+                    q|Q)
+                        printf "\nProgram terminated\n"
+                        exit
+                        ;;
+                    n|N)
+                        interactive
+                        ;;
+                    *)
+                        printf "\n${RED}[!]${NOCOLOR} Invalid option\n"
+                        read -p "PRESS ENTER TO CONTINUE"
+                        continue
+                        ;;
+                esac
+        else
+            printf "${RED}[!]${NOCOLOR} Wrong formats:\n\tsite: '%s'\n\tnextcloud_directory: '%s'\n" "$SITE" "$NEXTCLOUD_DIR"
+            interactive
+            continue
+        fi
+    done
+
+    if mkdir "/var/www/html/${NEXTCLOUD_DIR}" && chown -R www-data:www-data "${NEXTCLOUD_DIR}" &&  chmod -R 770 "${NEXTCLOUD_DIR}"; then
+        printf "working"
+    else
+        printf "\nnot working\n" >&2
+    fi
+else
+    printf "\n${RED}[!]${NOCOLOR} Error: site or nextcloud directory undefined\n" >&2
+    exit 1
+fi
--- linux/system_information_htmlVersion.sh
+++ linux/system_information_htmlVersion.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # work in progress
-# Program to output a system information webpage
+# Program to output a system information html page
 : '
     ______                       _          ______                        __
    / ____/_  ______ ____  ____  (_)___     / ____/___  ____  ____  ____ _/ /__  ____
@@ -13,9 +13,6 @@
 '
 
 
-TITLE="System information report for $HOSTNAME"
-DATE="$(date +"%x %r %Z")"
-TIMESTAMP="Generado $DATE por el usuario $USER"
 
 report_uptime () {
     cat <<- _EOF_
@@ -28,72 +25,173 @@ _EOF_
 
 report_disk_space () {
     cat <<- _EOF_
-        <h2>System Uptime</h2>
+        <h2>Disk Space Utilization</h2>
         <pre>$(df -h)</pre>
 _EOF_
     return
 }
 
 report_home_space () {
+    local format i home_dir total_files total_directories total_size username
+    format="%8s%10s%10s\n"
     # I check if the user running the script is root or normal user 
-    if [[ "$(id -u)" -eq 0 ]]; then # if the user is root, I show the disk usage of the home folder 
-            cat <<- _EOF_ > ./report_user_home_space.html
-            <h2>Home Space Utilization (all users)</h2>
-            <pre>$(du -sh /home/*)</pre>
-_EOF_
+    if [[ "$(id -u)" -eq 0 ]]; then
+        home_dir="/home/*"
+        username='All users in the system'
     else
-        cat <<- _EOF_ > ./report_home_space.html
-        <h2>Home Space Utilization ($USER)</h2>
-        <pre>$(df -h)</pre>
-_EOF_
+        home_dir="$HOME"
+        username="$USER"
     fi
+    
+    echo "<h2>Home Space Utilization: ($username)</h2>"
+
+    for i in $home_dir; do
+        total_files="$(find "$i" -type f | wc -l)"
+        total_directories="$(find "$i" -type d | wc -l)"
+        total_size="$(du -sh "$i" | cut -f 1)"
+
+        echo "<h3>$i</h3>"
+        echo "<pre>"
+        printf "$format" "Dirs" "Files" "Size"
+        printf "$format" "----" "----" "----"
+        printf "$format" "$total_directories" "$total_files" "$total_size"
+        echo "</pre>"
+    done
+    return
+}
+# user processes
+report_user_process_list () {
+    return
+}
+
+# HTML PAGE
+html_page () {
+    local TITLE DATE TIMESTAMP
+    TITLE="System information report for $HOSTNAME"
+    DATE="$(date +"%x %r %Z")"
+    TIMESTAMP="Gerated $DATE, by $USER"
+    cat <<- _EOF_
+    <html>
+        <head>
+            <title>$TITLE</title>
+        </head>
+        <body>
+            <h1>$TITLE</h1>
+            <p>$TIMESTAMP</p>
+            $(report_uptime)
+            $(report_disk_space)
+            $(report_home_space)
+        </body>
+    </html>
+_EOF_
+    return
+}
+
+usage () {
+    SCRIPTNAME="$(basename "$0")"
+    echo "${SCRIPTNAME} - usage: ${SCRIPTNAME} [-f file | -i]"
 }
 
 
-cat << _EOF_
-<html>
-    <head>
-        <title>$TITLE</title>
-    </head>
-    <body>
-        <h1>$TITLE</h1>
-        <p>$TIMESTAMP</p>
-    </body>
-</html>
-_EOF_
-
-
 # === Menu ===
-while true; do
-    clear
-    cat <<- _EOF_
-        Select:
-        
-        0. Quit
-        1. Display system information
-        2. Display disk space
-        3. Display home space utilization
-    
-_EOF_
-    
-    read -p "Choose one > " Opt
-    if [[ "$Opt" =~ ^[0-3]$ ]]; then
-        if [[ "$Opt" == 0 ]]; then
-            echo 'Bye! :D'
-            exit
-        elif [[ "$Opt" == 1 ]]; then
-            report_uptime
-            read -rp "[*] Press enter to continue"
-        elif [[ "$Opt" == 2 ]]; then
-            report_disk_space
-            read -rp "[*] Press enter to continue"
-        elif [[ "$Opt" == 3 ]]; then
-            report_home_space
-            read -rp "[*] Press enter to continue"
-        fi
-    else
-        echo '[!] Error: invalid entry'
-        read -rp "[*] Press enter to continue"
-        continue
-    fi 
+# while true; do
+#     clear
+#     cat <<- _EOF_
+#         Select:
+#         
+#         0. Quit
+#         1. Display system information
+#         2. Display disk space
+#         3. Display home space utilization
+#     
+# _EOF_
+#     
+#     read -p "Choose one > " Opt
+#     if [[ "$Opt" =~ ^[0-3]$ ]]; then
+#         case "$Opt" in
+#             0)
+#                 echo 'Bye! :D'
+#                 exit
+#                 ;;
+#             1)
+#                 report_uptime
+#                 read -rp "[*] Press enter to continue"
+#                 ;;
+#             2)
+#                 report_disk_space
+#                 read -rp "[*] Press enter to continue"
+#                 ;;
+#             3)
+#                 report_home_space
+#                 read -rp "[*] Press enter to continue"
+#                 ;;
+#         esac
+#     else
+#         echo '[!] Error: invalid entry'
+#         read -rp "[*] Press enter to continue"
+#         continue
+#     fi 
+# done
+
+
+# === MAIN ===
+
+interactive=
+filename=
+while [[ -n "$1" ]]; do
+    case "$1" in
+        -f | --file)
+            shift
+            filename="$1"
+            ;;
+        -i | --interactive)
+            interactive=1
+            ;;
+        -h | --help)
+            usage
+            ;;
+        *)
+            usage >&2
+            exit 1
+            ;;
+    esac
+    shift
 done
+
+# interactive mode
+if [[ -n "$interactive" ]]; then
+    while true; do
+        read -p "Enter the name of output file: " filename
+        if [[ -f "$filename" ]]; then
+            read -p "'$filename' exists. Overwrite? [y|n|q] > "
+            case "$REPLY" in
+                Y|y)
+                    break
+                    ;;
+                q|Q)
+                    echo "Program terminated"
+                    exit
+                    ;;
+                *)
+                    continue
+                    ;;
+            esac
+        elif [[ -z "$filename" ]]; then
+            continue
+        else
+            break
+        fi
+    done
+fi
+
+
+if [[ -n "$filename" ]]; then
+    if touch "$filename" && [[ -f "$filename" ]]; then
+        html_page > "$filename"
+    else
+        echo "Cannot write file '$filename'" >&2
+        exit 1
+    fi
+else # output by default
+    html_page
+fi
